a62c01b1ed3559fd0eacd0b08d54e7150488550e
Wali Morris <walimmorris@gmail.com>
2024-04-23 17:43:40 -0700

    add Lombok to Address.java and Contact.java

--- backend/src/main/java/com/morris/aurum/models/Address.java
+++ backend/src/main/java/com/morris/aurum/models/Address.java
@@ -1,5 +1,10 @@
 package com.morris.aurum.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Address {
     String primaryNumber;
     String streetName;
@@ -7,52 +12,4 @@ public class Address {
     String city;
     String state;
     String zipcode;
-
-    public String getPrimaryNumber() {
-        return primaryNumber;
-    }
-
-    public void setPrimaryNumber(String primaryNumber) {
-        this.primaryNumber = primaryNumber;
-    }
-
-    public String getStreetName() {
-        return streetName;
-    }
-
-    public void setStreetName(String streetName) {
-        this.streetName = streetName;
-    }
-
-    public String getStreetSuffix() {
-        return streetSuffix;
-    }
-
-    public void setStreetSuffix(String streetSuffix) {
-        this.streetSuffix = streetSuffix;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
 }
--- backend/src/main/java/com/morris/aurum/models/Contact.java
+++ backend/src/main/java/com/morris/aurum/models/Contact.java
@@ -1,31 +1,12 @@
 package com.morris.aurum.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Contact {
     String countryCode;
     String areaCode;
     String primaryNumber;
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getAreaCode() {
-        return areaCode;
-    }
-
-    public void setAreaCode(String areaCode) {
-        this.areaCode = areaCode;
-    }
-
-    public String getPrimaryNumber() {
-        return primaryNumber;
-    }
-
-    public void setPrimaryNumber(String primaryNumber) {
-        this.primaryNumber = primaryNumber;
-    }
 }
